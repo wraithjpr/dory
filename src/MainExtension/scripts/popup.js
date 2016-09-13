78e86e12f3823f60cd95cca9e132e6bb68c4caac
james.wraith <james.wraith@securus-software.com>
2016-09-13 17:59:47 +0100

    WebRequestMonitor and MainExtension communicating with single messages.

--- src/MainExtension/scripts/popup.js
+++
@@ -1,7 +0,0 @@
-;(function (window, document, global) { // eslint-disable-line no-extra-semi
-  'use strict';
-
-  global.$$popup$$ = 'Hello from popup!';
-
-  return;
-}(window, document, window));
--- src/WebRequestMonitor/scripts/background.js
+++ src/WebRequestMonitor/scripts/background.js
@@ -1,6 +1,8 @@
 ;(function (window, document, global, chrome) { // eslint-disable-line no-extra-semi
   'use strict';
 
+  const MAIN_EXTENSION_ID = 'abdgnfkllabhagajifbiladleedklkep';
+
   const RESOURCE_TYPES_ENUM = [ // eslint-disable-line no-unused-vars
     'font',
     'image',
@@ -44,22 +46,48 @@
     types: RESOURCE_TYPES_ENUM
   };
 
-  const formatTimeStamp = (timeStamp) => (new Date(timeStamp)).toISOString();
+  const responseCallback = function (response) {
+    if (response) {
+      console.log(`${response.ack ? 'ACK' : response.nack ? 'NACK' : 'ERROR'}`);  // eslint-disable-line no-nested-ternary
+    } else {
+      if (chrome.runtime.lastError) {  // eslint-disable-line no-lonely-if
+        console.log(chrome.runtime.lastError.message);
+      } else {
+        console.log('Expected a chrome.runtime.lastError but none existed.');
+      }
+    }
+
+    return;
+  };
+
+  const sendToMain = function (message) {
+    chrome.runtime.sendMessage(MAIN_EXTENSION_ID, message, responseCallback);
+
+    return message;
+  };
+
+  const makeMessageFromPayload = (type, payload) => ({
+    type,
+    payload
+  });
+  const makeBlockMessage = (payload) => makeMessageFromPayload('block', payload);
+  const makeRedirectMessage = (payload) => makeMessageFromPayload('redirect', payload);
+  const makeMonitorMessage = (payload) => makeMessageFromPayload('monitor', payload);
 
   const listenAllCallback = function (details) {
-    console.log(`Web request ${details.requestId} of type ${details.type}: ${details.method} ${details.url} at ${formatTimeStamp(details.timeStamp)}`);
+    sendToMain(makeMonitorMessage(details));
 
     return null;
   };
 
   const redirectFacebookCallback = function (details) {
-    console.log(`Facebook redirected: ${details.url}`);
+    sendToMain(makeRedirectMessage(details));
 
     return REDIRECT_TO_SECURUS_RESPONSE;
   };
 
   const blockBlacklistCallback = function (details) {
-    console.log(`Blacklist intercepted: ${details.url}`);
+    sendToMain(makeBlockMessage(details));
 
     return BLOCK_RESPONSE;
   };
@@ -67,6 +95,9 @@
   chrome.webRequest.onBeforeRequest.addListener(listenAllCallback, listenAllFilter);
   chrome.webRequest.onBeforeRequest.addListener(redirectFacebookCallback, redirectFacebookFilter, OPTIONS_BLOCK);
   chrome.webRequest.onBeforeRequest.addListener(blockBlacklistCallback, blockBlacklistFilter, OPTIONS_BLOCK);
+
+  // Say hello to the main extension
+  sendToMain({ payload: 'Hello there, Main!... from The Monitor' });
 
   return;
 }(window, document, window, chrome));
